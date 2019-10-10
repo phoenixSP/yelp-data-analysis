81dccd8787a8a4ebcaa0a45abcd373b0da9a03be
jaamaa <jaamaa8@gmail.com>
2019-10-10 09:37:09 -0500

    handling error

--- src/json_to_xml.py
+++ src/json_to_xml.py
@@ -9,20 +9,23 @@ source_dataset_path = "../data/dataset_no_nulls/"
 target_dataset_path = "../data/dataset_xml/"
 
 files = ['business', 'tip' , 'review','checkin','tip','user','photo']
-#files = ['business', 'tip']
 for f in files:
 	print("-----Running "+f+" -----")
-
+	error_counter = 0;
 	outfile = open(target_dataset_path + f+'.xml', 'wb')
 	outfile.write(str.encode('<?xml version="1.0" encoding="UTF-8" ?>'))
 	outfile.write(str.encode('<root>'))
 	with open(source_dataset_path + f +'_no_nulls.json', "r") as ins:
 		for line in ins:
-			json_obj = json.loads(line)
-			xml = dicttoxml.dicttoxml(json_obj, custom_root='business', root=False)
-			outfile.write(str.encode("<"+f+">\n"))
-			outfile.write(xml)
-			outfile.write(str.encode("</"+f+">\n"))
+			try:
+				json_obj = json.loads(line)
+				xml = dicttoxml.dicttoxml(json_obj, root=False)
+				outfile.write(str.encode("<"+f+">\n"))
+				outfile.write(xml)
+				outfile.write(str.encode("</"+f+">\n"))
+			except ValueError:
+				error_counter +=1
 	outfile.write(str.encode('</root>'))
+	print(f+" number of erros occured: " + str(error_counter))
 	print("-----Done "+f+" -----")
 	# subprocess.call(['mv',target_dataset_path + f+'_not_nulls.json','cleandata'])
