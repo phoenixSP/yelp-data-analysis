ae6bb6d730bf1eaff961268b1725905788a1524c
phoenixSP <pal00007@cs-saxophone.cs.umn.edu>
2019-10-28 19:49:48 -0500

    adding the support and confidence part

--- src/advanced2_processing.py
+++ src/advanced2_processing.py
@@ -20,8 +20,42 @@ tree = ET.parse(extracted_business_info)
 
 root = tree.getroot()
 
+all_categories = set()
+
 for child in tree.iter('categories'):
-    print(child.text[0])
+    cat = child.text.split(",")
+    #print(cat)
+    for e in cat: 
+        all_categories.add(e.strip())
+            
+#%%
+        
+all_categories_list = []
+
+for child in tree.iter('categories'):
+    cat = child.text.split(",")
     
-    #process the string and parse as elements seperated by comma
-    #store it in a set/dictinary 
+    def strip(s):
+        return s.strip()
+    
+    cat = list(map(strip, cat))
+    all_categories_list.append(cat)
+    
+    
+    
+#%%
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+
+te = TransactionEncoder()
+te_ary = te.fit(all_categories_list).transform(all_categories_list)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+frequent_itemsets = apriori(df, min_support=0.1, use_colnames=True)   
+
+#%%
+
+res = association_rules(frequent_itemsets, metric="confidence", min_threshold=1)
+
+#%%
