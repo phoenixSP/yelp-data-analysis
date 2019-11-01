3b6f80a79b81b44449653f2d8b71e11e0eeeb170
phoenixSP <pal00007@csel-kh4250-15.cselabs.umn.edu>
2019-11-01 11:51:45 -0500

    adding visualization food

--- src/visualization.py
+++ src/visualization.py
@@ -6,27 +6,54 @@ Created on Thu Oct 31 20:47:51 2019
 @author: pal00007
 """
 
-import gmplot 
+from gmplot import *
 import os
-
+import geopandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
 #%%
 data_folder = "/home/pal00007/Documents/big_data/CSCI5751/data"
 
-#%%
-gmap = gmap = gmplot.GoogleMapPlotter(37.428, -130.145, 4)
-gmap.draw(os.path.join(data_folder,"map.html"))
 
-#%%
-import json
+#https://www.geeksforgeeks.org/python-plotting-google-map-using-gmplot-package/
+gmap = gmplot.GoogleMapPlotter(37.428, -130.145, 4)
+gmap.apikey = 'AIzaSyArIFr43eixG87ARefxdcv06OohPrGwYfE'
+
+
+
 with open(os.path.join(data_folder,"business_latlong_json.json")) as location_data:
     data = json.load(location_data)
     
 #%%
 locations = data['Food']
 
-#%%
+
 locations_tofloat = [list(map(float, i)) for i in locations]
-locations_usa = [[i, j] for [i,j] in locations_tofloat if 25 <= i <=50 and -130 <= j <= -70 ]
+locations_usa = [[i, j] for [i,j] in locations_tofloat if 19 <= i <=64 and -162 <= j <= -70 ]
+
+
+
+
+locations_usa = np.array(locations_usa)
+latitude = locations_usa[:,0]
+longitude = locations_usa[:,1]
+
+
+
+gmap.scatter( latitude[:20], longitude[:20])
+gmap.draw(os.path.join(data_folder,"map.html"))
 
 #%%
-
+#Attempt using GeoPandas
+#data = {'latitude': latitude[: 100], 'longitude': longitude[:100]}
+#pd_data = pd.DataFrame.from_dict(data)
+#gdf = geopandas.GeoDataFrame(pd_data, geometry= geopandas.points_from_xy(pd_data.longitude, pd_data.latitude))
+#
+#world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+#ax = world[world.continent == 'North America'].plot(color = 'white', edgecolor = 'black')
+## We can now plot our ``GeoDataFrame``.
+#gdf.plot(ax=ax, color='red', figsize= (8,8))
+#
+#plt.show()
